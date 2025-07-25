c2b949b3d73e99210eb499901be72908a3578abe
Mickael <61110922+MickaelMd@users.noreply.github.com>
2025-07-25 11:19:11 +0200

    update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\DetailCommande;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,6 +18,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Fournisseur;
 use app\Entity\Produit;
 use app\Entity\SousCategorie;
+use App\Repository\DetailCommandeRepository;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 
@@ -34,11 +36,25 @@ final class AdminController extends AbstractController
 
         $fournisseur = $fournisseurRepository->findBy([], ['nom' => 'ASC']);
         $produit = $produitRepository->findBy([], ['libelleCourt' => 'ASC']);
-        $commande = $commandeRepository->findAll();
          $categorie = $categorieRepository->findBy(['active' => true], ['nom' => 'ASC']);
         foreach ($categorie as $cat) {
             $cat->getSousCategories()->count(); 
         }
+
+        $commande = $commandeRepository->createQueryBuilder('c')
+            ->where('c.statu != :statu')
+            ->setParameter('statu', 'Livrée')
+            ->orderBy('c.dateCommande', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+
+                
+            foreach ($commande as $cmd) {
+                foreach ($cmd->getDetailCommandes() as $detail) {
+                    $produit = $detail->getProduit(); 
+                }
+            }
 
 
         $qb = $utilisateurRepository->createQueryBuilder('u');
@@ -363,4 +379,112 @@ public function updateProduit(
             return $this->redirectToRoute('app_admin');
         }
     }
+
+
+
+    #[Route('/admin-update-commande', name: 'app_admin_update_commande', methods: ['POST'])]
+    public function adminUpdateCom(CsrfTokenManagerInterface $csrfTokenManager, Request $request, CommandeRepository $commandeRepository, DetailCommandeRepository $detailComRepo,  EntityManagerInterface $entityManager): Response
+    {
+
+         if (!$this->isGranted('ROLE_COMMERCIAL') && !$this->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('error', 'Vous n\'avez pas les droits pour accéder à cette page.');
+            return $this->redirectToRoute('app_accueil');
+        }
+        
+        $token = new CsrfToken('authenticate_commande', $request->request->get('_csrf_token'));
+        if (!$csrfTokenManager->isTokenValid($token)) {
+            throw new \Exception('Jeton CSRF invalide.');
+        }
+
+            $tabValue = ['en_attente', 'en_préparation', 'expédiée', 'livrée'];
+            
+            $id  = $request->request->get('com_id');
+            $select = $request->request->get('statut');
+            $commande = $commandeRepository->find($id);
+            $detailCommande = $detailComRepo->findBy(['Commande' => $commande]);
+
+            if (!$commande) {
+                $this->addFlash('error', 'Commande introuvable.');
+                return $this->redirectToRoute('app_admin');
+            }
+
+            if (!in_array($select, $tabValue, true)) {
+                    $this->addFlash('error', 'Erreur dans le formulaire.');
+                    return $this->redirectToRoute('app_admin');
+                }
+ 
+        try {
+
+          foreach ($detailCommande as $key) {
+            
+            $key->setStatut($select);
+            $entityManager->persist($key);
+            $entityManager->flush();
+
+          }
+                $commande->setStatu($select);
+                $entityManager->persist($commande);
+                $entityManager->flush();
+
+         } catch(\Exception $e) {
+            $this->addFlash('error', 'Une erreur s\'est produite, veuillez réessayer.');
+            return $this->redirectToRoute('app_admin');
+         }
+         
+        return $this->redirectToRoute('app_admin');
+        
+    }
+
+    // --------------------
+
+        #[Route('/admin-update-det_com', name: 'app_admin_update_det_com', methods: ['POST'])]
+    public function adminUpdateDetCom(CsrfTokenManagerInterface $csrfTokenManager, Request $request, CommandeRepository $commandeRepository, DetailCommandeRepository $detailComRepo,  EntityManagerInterface $entityManager): Response
+    {
+
+         if (!$this->isGranted('ROLE_COMMERCIAL') && !$this->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('error', 'Vous n\'avez pas les droits pour accéder à cette page.');
+            return $this->redirectToRoute('app_accueil');
+        }
+        
+        $token = new CsrfToken('update_detail_commande', $request->request->get('update_detail_commande'));
+        if (!$csrfTokenManager->isTokenValid($token)) {
+            throw new \Exception('Jeton CSRF invalide.');
+        }
+
+            $tabValue = ['en_attente', 'en_préparation', 'expédiée', 'livrée'];
+
+            $id  = $request->request->get('com-commande-produit-id');
+            $select = $request->request->get('statut-produit');
+            $detCommande = $detailComRepo->find($id);
+            
+            $commande = $detCommande->getCommande();
+
+
+
+            if (!$detCommande) {
+                $this->addFlash('error', 'Commande introuvable.');
+                return $this->redirectToRoute('app_admin');
+            }
+
+            if (!in_array($select, $tabValue, true)) {
+                    $this->addFlash('error', 'Erreur dans le formulaire.');
+                    return $this->redirectToRoute('app_admin');
+                }
+ 
+        try {
+
+                $detCommande->setStatut($select);
+                $entityManager->persist($detCommande);
+                $entityManager->flush();
+
+         } catch(\Exception $e) {
+            $this->addFlash('error', 'Une erreur s\'est produite, veuillez réessayer.');
+            return $this->redirectToRoute('app_admin');
+         }
+         
+        return $this->redirectToRoute('app_admin');
+        
+    }
+
+
 }
